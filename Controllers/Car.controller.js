fb1dcaeb441c5767990d28be8c73b879317c4243
Sharik <md.sharik5012@gmail.com>
2024-04-23 19:57:37 +0530

    av

--- Controllers/Car.controller.js
+++ Controllers/Car.controller.js
@@ -2,10 +2,21 @@ import Car from "../models/carState.model.js";
 
 const getAllCars = async (req, res) => {
   try {
-    const cars = await Car.find();
+    const category = req.query.category;
+    // Access the 'category' query parameter
+    let cars;
+
+    // If category is provided, filter cars by category
+    if (category) {
+      cars = await Car.find({ category: category });
+    } else {
+      cars = await Car.find();
+    }
+    console.log("Filtered cars:", cars);
     res.status(200).json(cars);
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
@@ -16,6 +27,7 @@ const createCar = async (req, res) => {
     res.status(201).json(car);
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
